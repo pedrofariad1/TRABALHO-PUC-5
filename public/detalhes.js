e8d2e59ec8a4ac47f56ad02946d86f8483a6b699
Pedro Henrique Faria <pedro.godinho.1658056@sga.pucminas.br>
2026-05-16 23:05:13 -0300

    Trabalho Pratico 1 Finalizado

--- public/detalhes.js
+++ public/detalhes.js
@@ -11,7 +11,17 @@ const data = {
       categoria: "Processadores",
       imagem: "https://imgs.search.brave.com/BRLkRXGdnp1MqTlGfLEXL9QuxsNNM6iPXytsrd_sFkk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9wY2Rp/Z2EtcHJvZC5ldS5z/YWxlb3IuY2xvdWQv/bWVkaWEvdGh1bWJu/YWlscy9wcm9kdWN0/cy8xX3AwNjQ4OTdf/MV9hMDIyOGNiM190/aHVtYm5haWxfMTAy/NC5qcGc",
       descricao: "Processador rápido para jogos",
-      emEstoque: true
+      emEstoque: true,
+      fotos: [
+        {
+          titulo: "Imagem frontal",
+          imagem: "https://imgs.search.brave.com/EOQGnio5GCIfMzFC51adimP7QoC4QanOcF6Cmj1xDTE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jMS5u/ZXdlZ2dpbWFnZXMu/Y29tL3Byb2R1Y3Rp/bWFnZS9uYjY0MC8x/OS0xMTMtODE0LTA0/LmpwZw"
+        },
+        {
+          titulo: "Imagem traseira",
+          imagem: "https://images5.kabum.com.br/produtos/fotos/520365/processador-amd-ryzen-5-8600g-100-100001237box_1706637582_gg.jpg"
+        }
+      ]
     },
     {
       id: 2,
@@ -20,67 +30,143 @@ const data = {
       categoria: "Placas de Vídeo",
       imagem: "https://imgs.search.brave.com/O5fYCBBS3c9L1kPQ0Al4wRgqtqrlmjNOBZvf1o9OKJM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9mdWpp/b2thZGlzdHJpYnVp/ZG9yLnZ0ZXhpbWcu/Y29tLmJyL2FycXVp/dm9zL2lkcy8xOTk1/NTMtMjkyLTI5Mi9w/XzU1MTU3X2FsdGFf/NC5wbmc_dj02Mzc2/ODAyMjkzNDc3NzAw/MDA",
       descricao: "Alta performance gráfica",
-      emEstoque: true
-    },
+      emEstoque: true,
+      fotos: [
+        {
+          titulo: "Imagem frontal",
+          imagem: "https://http2.mlstatic.com/D_NQ_NP_2X_876194-MLU72567976318_112023-F.webp"
+        },
+        {
+          titulo: "Produto em detalhe",
+          imagem: "https://http2.mlstatic.com/D_NQ_NP_2X_864312-MLA52925614508_122022-F.webp"
+        }
+      ]
+    }
+      ,
     {
       id: 3,
       nome: "Memória RAM 16GB",
       preco: 349,
       categoria: "Memória",
-      imagem: "https://imgs.search.brave.com/h5f4J6n9CdQ1t2YZfhcxoScDzWb419cyIbI0PEWsHcw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnpz/dC5jb20uYnIvdGh1/bWJzLzUxLzM0LzFj/Ly0xNTQ3Mzg1NzQ1/LmpwZw",
+      imagem: "https://images6.kabum.com.br/produtos/fotos/172366/memoria-kingston-fury-beast-16gb-3200mhz-ddr4-cl16-preto-kf432c16bb1-16_1626271100_gg.jpg",
       descricao: "Alta velocidade",
-      emEstoque: true
+      emEstoque: true,
+      fotos: [
+        {
+          titulo: "Imagem frontal",
+          imagem: "https://images6.kabum.com.br/produtos/fotos/172366/memoria-kingston-fury-beast-16gb-3200mhz-ddr4-cl16-preto-kf432c16bb1-16_1626271101_gg.jpg"
+        },
+        {
+          titulo: "Detalhes do produto",
+          imagem: "https://images6.kabum.com.br/produtos/fotos/172366/memoria-ram-kingston-fury-beast-16gb-3200mhz-ddr4-cl16-preto-kf432c16bb1-16_1743593467_gg.jpg"
+        }
+      ]
     },
+
     {
       id: 4,
       nome: "Gabinete Gamer",
       preco: 249,
       categoria: "Gabinetes",
-      imagem: "https://imgs.search.brave.com/_gfa5L68wEgGHurj0uDqcLOsDBr6Z2V56qMi1dOHHe4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9wY2J1/aWxkZXIuY29tLmJy/L19uZXh0L2ltYWdl/P3VybD1odHRwczov/L29ibHNqcGFhaWl0/dnhxdGpqbmttLnN1/cGFiYXNlLmNvL3N0/b3JhZ2UvdjEvb2Jq/ZWN0L3B1YmxpYy9t/b25hZC1pbWFnZXMv/cHJvZHV0b3MvYTcz/ZTJkNzEtM2JiMS00/MjY0LWJiOTktZDBk/MTI0YWFhMjZjLzAu/anBnJnc9Mzg0MCZx/PTc1",
+      imagem: "https://images5.kabum.com.br/produtos/fotos/sync_mirakl/997445/xlarge/PC-Gamer-Gabinete-Aquario-Preto-Ryzen-5-5500-RTX-5060-8GB-16gb-Ram-SSD-500GB-Water-Cooler-240mm-Win11-Pro_1768418914.jpg",
       descricao: "Design moderno",
-      emEstoque: false
+      emEstoque: false,
+      fotos: [
+        {
+          titulo: "Imagem frontal",
+          imagem: "https://images5.kabum.com.br/produtos/fotos/sync_mirakl/997445/xlarge/PC-Gamer-Gabinete-Aquario-Preto-Ryzen-5-5500-RTX-5060-8GB-16gb-Ram-SSD-500GB-Water-Cooler-240mm-Win11-Pro_1768418917.jpg"
+        },
+        {
+          titulo: "Imagem Ilustrativa",
+          imagem: "https://images5.kabum.com.br/produtos/fotos/sync_mirakl/997445/xlarge/PC-Gamer-Gabinete-Aquario-Preto-Ryzen-5-5500-RTX-5060-8GB-16gb-Ram-SSD-500GB-Water-Cooler-240mm-Win11-Pro_1768418918.jpg"
+        }
+      ]
     },
+
     {
       id: 5,
       nome: "SSD 1TB",
       preco: 499,
       categoria: "Armazenamento",
-      imagem: "https://imgs.search.brave.com/c6mGCpbBEi3IOfkt1u3hU2jbfCcCXZBSE3H-zU1BFOs/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NjFjeUhxQitLbUwu/anBn",
+      imagem: "https://images7.kabum.com.br/produtos/fotos/95217/95217_1520016025_gg.jpg",
       descricao: "Muito rápido",
-      emEstoque: true
+      emEstoque: true,
+      fotos: [
+        {
+          titulo: "Imagem traseira",
+          imagem: "https://images7.kabum.com.br/produtos/fotos/95217/95217_1_1520016025_gg.jpg"
+        },
+        {
+          titulo: "Detalhes do produto",
+          imagem: "https://images7.kabum.com.br/produtos/fotos/95217/ssd-kingston-a400-960gb-sata-leitura-500mb-s-gravacao-450mb-s-sa400s37-960g_1629126712_gg.jpg"
+        }
+      ]
     },
+
     {
       id: 6,
       nome: "Fonte 600W",
       preco: 299,
       categoria: "Fontes",
-      imagem: "https://imgs.search.brave.com/6jGZV7M9oWdHL67tOZYx4rwihxfluTuGAjg1Kh_wSv8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9odHRw/Mi5tbHN0YXRpYy5j/b20vRF9OUV9OUF84/MDIxODYtTUxVNzc2/Njc3OTkyODBfMDcy/MDI0LU8ud2VicA",
+      imagem: "https://img.terabyteshop.com.br/produto/g/fonte-corsair-vs600-600w-80-plus-cp-9020224-br_113684.png",
       descricao: "Energia estável",
-      emEstoque: true
+      emEstoque: true,
+      fotos: [
+        {
+          titulo: "Imagem frontal",
+          imagem: "https://img.terabyteshop.com.br/produto/g/fonte-corsair-vs600-600w-80-plus-cp-9020224-br_113676.png"
+        },
+        {
+          titulo: "Imagem traseira",
+          imagem: "https://img.terabyteshop.com.br/produto/g/fonte-corsair-vs600-600w-80-plus-cp-9020224-br_113683.png"
+        }
+      ]
     },
+
     {
       id: 7,
       nome: "Monitor 144Hz",
       preco: 899,
       categoria: "Monitores",
-      imagem: "https://imgs.search.brave.com/f4HtlKQRfyTU79de82uFraWRNJWckMzIN045M8miniI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NTE1c2xDakhXNUwu/anBn",
+      imagem: "https://images4.kabum.com.br/produtos/fotos/993244/monitor-gamer-msi-24-full-hd-144hz-1ms-ips-freesync-hdr-ready-ai-vision-preto-g242l-e14_1773236384_gg.jpg",
       descricao: "Imagem fluida",
-      emEstoque: true
+      emEstoque: true,
+      fotos: [
+        {
+          titulo: "Imagem frontal",
+          imagem: "https://images4.kabum.com.br/produtos/fotos/993244/monitor-gamer-msi-24-full-hd-144hz-1ms-ips-freesync-hdr-ready-ai-vision-preto-g242l-e14_1773236386_gg.jpg"
+        },
+        {
+          titulo: "Imagem traseira",
+          imagem: "https://images4.kabum.com.br/produtos/fotos/993244/monitor-gamer-msi-24-full-hd-144hz-1ms-ips-freesync-hdr-ready-ai-vision-preto-g242l-e14_1773236385_gg.jpg"
+        }
+      ]
     },
+
     {
       id: 8,
       nome: "Teclado Mecânico",
       preco: 199,
       categoria: "Acessórios",
-      imagem: "https://imgs.search.brave.com/Ip2PkCwd47lsUcx7zJ3o5VUZFT-EaegGEQ9qL3AKCQk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9odHRw/Mi5tbHN0YXRpYy5j/b20vRF9RX05QXzJY/XzkzMzE3Mi1NTEE5/OTQ5NDQ5NTg2MF8x/MTIwMjUtRS53ZWJw",
+      imagem: "https://images4.kabum.com.br/produtos/fotos/535604/teclado-mecanico-gamer-machenike-k500-b61-switch-brown-abnt-branco-k500-b61bbr_1769021519_gg.jpg",
       descricao: "Teclas rápidas",
-      emEstoque: true
+      emEstoque: true,
+      fotos: [
+        {
+          titulo: "Imagem frontal",
+          imagem: "https://images4.kabum.com.br/produtos/fotos/535604/teclado-mecanico-gamer-machenike-k500-b61-switch-brown-abnt-branco-k500-b61bbr_1769021517_gg.jpg"
+        },
+        {
+          titulo: "Detalhes do produto",
+          imagem: "https://images4.kabum.com.br/produtos/fotos/535604/teclado-mecanico-gamer-machenike-k500-b61-switch-brown-abnt-branco-k500-b61bbr_1769021517_gg.jpg"
+        }
+      ]
     }
   ]
 };
 
 // =======================
-// PEGAR ID DA URL
+// PEGAR ID URL
 // =======================
 
 const params = new URLSearchParams(window.location.search);
@@ -93,7 +179,7 @@ const id = Number(params.get("id"));
 const produto = data.produtos.find(p => p.id === id);
 
 // =======================
-// MOSTRAR NA TELA
+// DOM
 // =======================
 
 const container = document.getElementById("detalhes-produto");
@@ -102,24 +188,30 @@ function formatPrice(preco) {
   return "R$ " + preco.toFixed(2);
 }
 
+// =======================
+// RENDER
+// =======================
+
 if (produto) {
 
   container.innerHTML = `
   
     <div class="card p-4 shadow">
 
-      <div class="row">
+      <div class="row g-4">
 
-        <div class="col-md-6">
-          <img src="${produto.imagem}" 
-               class="img-fluid rounded">
+        <div class="col-12 col-lg-6">
+
+          <img src="${produto.imagem}"
+              class="img-fluid rounded w-100">
+
         </div>
 
-        <div class="col-md-6">
+        <div class="col-12 col-lg-6">
 
           <h2>${produto.nome}</h2>
 
-          <p class="fs-4 fw-bold text-primary">
+          <p class="fs-3 fw-bold text-primary">
             ${formatPrice(produto.preco)}
           </p>
 
@@ -135,10 +227,44 @@ if (produto) {
 
           <p>
             <strong>Status:</strong>
-            ${produto.emEstoque ? "Em estoque" : "Sem estoque"}
+            ${produto.emEstoque
+              ? "Em estoque"
+              : "Sem estoque"}
           </p>
 
         </div>
+
+      </div>
+
+      <hr class="my-4">
+
+      <h3 class="mb-4">
+        Fotos do Produto
+      </h3>
+
+      <div class="row">
+
+        ${produto.fotos.map(foto => `
+        
+          <div class="col-12 col-md-6 col-lg-4 mb-4">
+
+            <div class="card h-100 shadow-sm">
+
+              <img src="${foto.imagem}"
+                  class="card-img-top"
+                  style="height:220px; object-fit:cover;">
+
+              <div class="card-body text-center">
+
+                <h5>${foto.titulo}</h5>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        `).join("")}
 
       </div>
 
@@ -148,6 +274,8 @@ if (produto) {
 } else {
 
   container.innerHTML = `
-    <h2>Produto não encontrado</h2>
+    <div class="alert alert-danger">
+      Produto não encontrado.
+    </div>
   `;
 }
--- public/script.js
+++ public/script.js
@@ -11,7 +11,8 @@ const data = {
       categoria: "Processadores",
       imagem: "https://imgs.search.brave.com/BRLkRXGdnp1MqTlGfLEXL9QuxsNNM6iPXytsrd_sFkk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9wY2Rp/Z2EtcHJvZC5ldS5z/YWxlb3IuY2xvdWQv/bWVkaWEvdGh1bWJu/YWlscy9wcm9kdWN0/cy8xX3AwNjQ4OTdf/MV9hMDIyOGNiM190/aHVtYm5haWxfMTAy/NC5qcGc",
       descricao: "Processador rápido para jogos",
-      emEstoque: true
+      emEstoque: true,
+      destaque: true 
     },
     {
       id: 2,
@@ -20,34 +21,38 @@ const data = {
       categoria: "Placas de Vídeo",
       imagem: "https://imgs.search.brave.com/O5fYCBBS3c9L1kPQ0Al4wRgqtqrlmjNOBZvf1o9OKJM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9mdWpp/b2thZGlzdHJpYnVp/ZG9yLnZ0ZXhpbWcu/Y29tLmJyL2FycXVp/dm9zL2lkcy8xOTk1/NTMtMjkyLTI5Mi9w/XzU1MTU3X2FsdGFf/NC5wbmc_dj02Mzc2/ODAyMjkzNDc3NzAw/MDA",
       descricao: "Alta performance gráfica",
-      emEstoque: true
+      emEstoque: true,
+      destaque: true
     },
     {
       id: 3,
       nome: "Memória RAM 16GB",
       preco: 349,
       categoria: "Memória",
-      imagem: "https://imgs.search.brave.com/h5f4J6n9CdQ1t2YZfhcxoScDzWb419cyIbI0PEWsHcw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnpz/dC5jb20uYnIvdGh1/bWJzLzUxLzM0LzFj/Ly0xNTQ3Mzg1NzQ1/LmpwZw",
+      imagem: "https://images6.kabum.com.br/produtos/fotos/172366/memoria-kingston-fury-beast-16gb-3200mhz-ddr4-cl16-preto-kf432c16bb1-16_1626271100_gg.jpg",
       descricao: "Alta velocidade",
-      emEstoque: true
+      emEstoque: true,
+      destaque: false
     },
     {
       id: 4,
       nome: "Gabinete Gamer",
       preco: 249,
       categoria: "Gabinetes",
-      imagem: "https://imgs.search.brave.com/_gfa5L68wEgGHurj0uDqcLOsDBr6Z2V56qMi1dOHHe4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9wY2J1/aWxkZXIuY29tLmJy/L19uZXh0L2ltYWdl/P3VybD1odHRwczov/L29ibHNqcGFhaWl0/dnhxdGpqbmttLnN1/cGFiYXNlLmNvL3N0/b3JhZ2UvdjEvb2Jq/ZWN0L3B1YmxpYy9t/b25hZC1pbWFnZXMv/cHJvZHV0b3MvYTcz/ZTJkNzEtM2JiMS00/MjY0LWJiOTktZDBk/MTI0YWFhMjZjLzAu/anBnJnc9Mzg0MCZx/PTc1",
+      imagem: "https://images5.kabum.com.br/produtos/fotos/sync_mirakl/997445/xlarge/PC-Gamer-Gabinete-Aquario-Preto-Ryzen-5-5500-RTX-5060-8GB-16gb-Ram-SSD-500GB-Water-Cooler-240mm-Win11-Pro_1768418914.jpg",
       descricao: "Design moderno",
-      emEstoque: false
+      emEstoque: false,
+      destaque: false
     },
     {
       id: 5,
       nome: "SSD 1TB",
       preco: 499,
       categoria: "Armazenamento",
-      imagem: "https://imgs.search.brave.com/c6mGCpbBEi3IOfkt1u3hU2jbfCcCXZBSE3H-zU1BFOs/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NjFjeUhxQitLbUwu/anBn",
+      imagem: "https://images7.kabum.com.br/produtos/fotos/95217/95217_1520016025_gg.jpg",
       descricao: "Muito rápido",
-      emEstoque: true
+      emEstoque: true,
+      destaque: false
     },
     {
       id: 6,
@@ -56,25 +61,28 @@ const data = {
       categoria: "Fontes",
       imagem: "https://imgs.search.brave.com/6jGZV7M9oWdHL67tOZYx4rwihxfluTuGAjg1Kh_wSv8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9odHRw/Mi5tbHN0YXRpYy5j/b20vRF9OUV9OUF84/MDIxODYtTUxVNzc2/Njc3OTkyODBfMDcy/MDI0LU8ud2VicA",
       descricao: "Energia estável",
-      emEstoque: true
+      emEstoque: true,
+      destaque: false
     },
     {
       id: 7,
       nome: "Monitor 144Hz",
       preco: 899,
       categoria: "Monitores",
-      imagem: "https://imgs.search.brave.com/f4HtlKQRfyTU79de82uFraWRNJWckMzIN045M8miniI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NTE1c2xDakhXNUwu/anBn",
+      imagem: "https://images4.kabum.com.br/produtos/fotos/993244/monitor-gamer-msi-24-full-hd-144hz-1ms-ips-freesync-hdr-ready-ai-vision-preto-g242l-e14_1773236384_gg.jpg",
       descricao: "Imagem fluida",
-      emEstoque: true
+      emEstoque: true,
+      destaque: true
     },
     {
       id: 8,
       nome: "Teclado Mecânico",
       preco: 199,
       categoria: "Acessórios",
-      imagem: "https://imgs.search.brave.com/Ip2PkCwd47lsUcx7zJ3o5VUZFT-EaegGEQ9qL3AKCQk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9odHRw/Mi5tbHN0YXRpYy5j/b20vRF9RX05QXzJY/XzkzMzE3Mi1NTEE5/OTQ5NDQ5NTg2MF8x/MTIwMjUtRS53ZWJw",
+      imagem: "https://images4.kabum.com.br/produtos/fotos/535604/teclado-mecanico-gamer-machenike-k500-b61-switch-brown-abnt-branco-k500-b61bbr_1769021519_gg.jpg",
       descricao: "Teclas rápidas",
-      emEstoque: true
+      emEstoque: true,
+      destaque: false
     }
   ]
 };
